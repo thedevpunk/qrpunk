32998c946f48619b038349690a4948e138eb121f
Andre <andrelindemail@gmail.com>
2023-11-10 00:11:07 +0100

    small visual fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-// import * as React from "react";
-
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -20,7 +18,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useEffect, useRef, useState } from "react";
+import { QrCode } from "lucide-react";
+import { useRef, useState } from "react";
 import QRCode from "react-qr-code";
 
 type Code = {
@@ -32,7 +31,7 @@ type Code = {
 };
 
 const defaultCode = {
-  url: "google.com",
+  url: "",
   size: 256,
   fgColor: "#000000",
   bgColor: "transparent",
@@ -43,7 +42,6 @@ export default function Home() {
   var codeRef = useRef<HTMLDivElement>(null);
 
   const [code, setCode] = useState<Code>(defaultCode);
-  const [url, setUrl] = useState("asd");
 
   const handleDonwloadClick = () => {
     const svg = codeRef.current!.querySelector("svg");
@@ -66,19 +64,6 @@ export default function Home() {
     };
 
     img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
-
-    //  if (!codeRef.current) return;
-
-    //  const canvas = codeRef.current.querySelector("svg");
-    //  console.log(canvas);
-    //  if (!canvas) return;
-    //  const image = canvas.toDataURL("image/png");
-    //  const link = document.createElement("a");
-    //  link.download = "qr-code.png";
-    //  link.href = image;
-    //  document.body.appendChild(link);
-    //  link.click();
-    //  document.body.removeChild(link);
   };
 
   const handleResetClick = () => {
@@ -89,7 +74,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <Card className="mw-[350px]">
         <CardHeader>
-          <CardTitle>Create QR-Code</CardTitle>
+          <div className="flex gap-1">
+            <QrCode className="text-emerald-600" />
+            <CardTitle>QR Punk</CardTitle>
+          </div>
           <CardDescription>Create your QR-Code easy as hell.</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-6">
@@ -161,7 +149,10 @@ export default function Home() {
               </Select>
             </div>
           </div>
-          <div ref={codeRef} className="p-6 bg-emerald-400">
+          <div
+            ref={codeRef}
+            className="p-6 bg-gradient-to-r from-teal-400 to-emerald-400 rounded"
+          >
             <QRCode
               value={code.url}
               fgColor={code.fgColor}
@@ -180,122 +171,3 @@ export default function Home() {
     </main>
   );
 }
-
-// import { Card, CardDescription, CardHeader } from "@/components/ui/card";
-// import Image from "next/image";
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <Card className="px-6 py-4">
-//         <CardHeader>QR-Code Generator</CardHeader>
-//         <CardDescription>Please create your QR-Code</CardDescription>
-//       </Card>
-//       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-//         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-//           Get started by editing&nbsp;
-//           <code className="font-mono font-bold">app/page.tsx</code>
-//         </p>
-//         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-//           <a
-//             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-//             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             By{' '}
-//             <Image
-//               src="/vercel.svg"
-//               alt="Vercel Logo"
-//               className="dark:invert"
-//               width={100}
-//               height={24}
-//               priority
-//             />
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-//         <Image
-//           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-//           src="/next.svg"
-//           alt="Next.js Logo"
-//           width={180}
-//           height={37}
-//           priority
-//         />
-//       </div>
-
-//       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-//         <a
-//           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Docs{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Find in-depth information about Next.js features and API.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Learn{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Learn about Next.js in an interactive course with&nbsp;quizzes!
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Templates{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Explore starter templates for Next.js.
-//           </p>
-//         </a>
-
-//         <a
-//           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-//           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <h2 className={`mb-3 text-2xl font-semibold`}>
-//             Deploy{' '}
-//             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//               -&gt;
-//             </span>
-//           </h2>
-//           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//             Instantly deploy your Next.js site to a shareable URL with Vercel.
-//           </p>
-//         </a>
-//       </div> */}
-//     </main>
-//   );
-// }
